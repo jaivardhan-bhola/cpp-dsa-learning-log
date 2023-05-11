772f12c087d9232ffc83a91c4c844225923a57e2
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2023-05-11 18:42:55 +0530

    update array.cpp

--- array.cpp
+++ array.cpp
@@ -124,13 +124,33 @@ void max_circular_sum_subarray(int a[], int n){
     cout<<max(nonwrapsum,wrapsum)<<endl;;    
 }
 
+bool pairsum(int a[], int n, int pairsum){
+    int low=0, high=n-1;
+    for(int i=0;i<n;i++){
+        while(low<high){
+            int sum=a[high]+a[low];
+            if(sum==pairsum){
+                cout<<low+1<<" "<<high+1<<endl;
+                return true;
+            }
+            else if(sum>pairsum){
+                high --;
+            }
+            else{
+                low++;
+            }
+        }
+    }
+    return false;
+}
+
 int main(){
-    int n;
-    cin>>n;
+    int n, ps;
+    cin>>n>>ps;
     int a[n];
     for (int i=0; i<n; i++){
         cin>>a[i];
     }
-    max_circular_sum_subarray(a,n);
+    cout<<pairsum(a,n,ps);
     return 0;
 }
